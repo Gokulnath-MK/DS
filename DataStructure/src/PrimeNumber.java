22eedde6360d498186983e6094d55ebaf9cc06fd
gokulnath_m <gokulnath_m@comcast.com>
2022-12-24 11:37:07 +0530

    Prime Number updated

--- DataStructure/src/PrimeNumber.java
+++ DataStructure/src/PrimeNumber.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     /**
-     there are 2 partions
-     sortedpartion
-     unsorted partions
+     * there are 2 partions
+     * sortedpartion
+     * unsorted partions
      **/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check whether its prime or not");
         int a = sc.nextInt();
         boolean prime = false;
-        if(a==1){
+        if (a == 1) {
             System.out.println("1 is neither prime nor composite");
             return;
-        }else {
-            for(int i = 2; i<= a ;i++){
-                if(a%i==0&& i == a){
+        } else {
+            for (int i = 2; i <= a; i++) {
+                if (a % i == 0 && i == a) {
                     prime = true;
                 }
             }
         }
-        if(prime){
+        if (prime) {
             System.out.println("Its prime");
-        }else{
+        } else {
             System.out.println("not prime");
         }
     }
